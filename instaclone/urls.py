4cfaf62326da8441b927d1f03f70c4389760e68d
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-03 16:59:37 +0300

    create base.html and extend it to index.html and navbar.html

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
