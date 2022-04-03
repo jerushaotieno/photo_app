fc73baa670a150d941a99700ac392eed8ca3eb0d
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-03 17:08:03 +0300

    connect to Postgres and create a database for the app

--- photoapp/settings.py
+++ photoapp/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'photoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instaclone',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 
