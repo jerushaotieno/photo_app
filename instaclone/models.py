7f95f15f24d3b4fec4d69d16864cfbfa56f83518
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-03 17:48:57 +0300

    create image and profile models

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# Image Model
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=30)
+    image_caption = models.TextField()
+    profile_foreign_key = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
+
+#  Profile Model
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='avatars/')
+    bio = models.TextField() 
