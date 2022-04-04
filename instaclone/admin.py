9527ba1c62951e330608e899931eeba010af08c5
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-04 22:38:20 +0300

    create superuser and add models to the admin page

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Image, Profile, Comments, FollowUser
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(FollowUser) 
