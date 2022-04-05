3d7cb9f83eef08d964bb3204f2f91b8c09e664a5
jerushaotieno <jerushaotienocoding@gmail.com>
2022-04-05 06:08:52 +0300

    search functionality added and working

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,11 +1,18 @@
+from django.conf import settings
 from django.conf.urls import url
+
+from photoapp.settings import STATIC_URL
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns=[
     url('^$',views.user_feed,name = 'user_feed'),
     url(r'^user_profile/$', views.user_profile, name='user_profile'),
+    url('^search/', views.search, name='search'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 
+
+# if settings.DEBUG:
+#     urlpatterns+=STATIC_URL(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- instaclone/views.py
+++ instaclone/views.py
@@ -11,3 +11,16 @@ def user_feed(request):
 def user_profile(request):
     return render(request, 'user_profile.html') 
 
+
+#  Search functionality
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_for = request.GET.get('instasearch')
+        result = Image.search_image(search_for)
+        message = f'{search_for}'
+
+        return render(request, 'search.html', {'message':message, 'result':result})
+    else:
+        message = 'Type something else to search for'
+        return render(request, 'search.html', {'message':message}) 
